0c2ebd650c7cd010a4f79f9ee9aa90903c86142e
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-13 07:22:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -69,3 +69,50 @@ class Song
    
    
 end 
+
+# Song
+#   #new
+#     takes in three arguments: a name, artist and genre
+#   - The way I was able to complete this first lab was to first create an instance class called Song. 
+#              class Dog 
+#               end 
+# than define initialize set to three arguments - name, artist, and genre.
+#         def initialize(name, artist, genre)
+#         end 
+#   #name
+#     has a name
+#    I than created a instant variable @name = name inside the initialize method - Every time an instance Song is created it will instantley provide the name of the song. 
+#   #artist
+#     has an artist
+#     After I  created a instant variable @artist = artist inside the initialize method - Every time an instance Song is created it will instantley provide the name of the artist.
+#   #genre
+#     has a genre
+#   After I  created a instant variable @genre = genre inside the initialize method.  - Every time an instance Song is created it will instantley provide the genre the artist belongs in.
+#   class variables
+#     has a class variable, @@count
+#     has a class variable, @@artists, that contains all of the artists of existing songs
+#     has a class variable, @@genres, that contains all of the genres of existingsongs
+#   .count
+#     is a class method that returns that number of songs created
+# I than had to create a class variable @@count = 0  that will take a count of the number of songs it has stored return the number of song it has created. In order for the class variable to keep count I than had to create a class method .count that returns the current count of albums.
+#         def self.count
+#              @@.count 
+#           end 
+# I than have to create a class variable @@count += 1 
+#   The count of albums should go up as soon as a new album is created, or initialized,  we are using the @@album_count class variable, inside of our #initialize method, which is an instance method. We are saying: when a new album is created, access the @@album_count class variable and increment its value by 1.
+#     the self keyword refers to the entire class itself, not to an instance of the class.
+#       A class variable is accessible to the entire class.
+#   .artists
+#     is a class method that returns a unique array of artists of existing songs
+#        I than had to create a class method artists and within the class method returned 
+#                 @@artists class variable 
+#                 def self.artists
+#                      @@artists.uniq
+#                    end 
+#The self. before the method name is a reminder that the method will not be running on a particular artist instance, but will be acting as the factory from which all artists are made: the Song class.
+#   .genres - 
+#     is a class method that returns a unique array of genres of existing songs
+#   .genre_count
+#     is a class method that returns a hash of genres and the number of songs that have those genres
+#   .artist_count
+#     is a class method that returns a hash of artists and the number of songs that have those artists
